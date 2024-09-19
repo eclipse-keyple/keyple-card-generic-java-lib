c09425f7a2de6ef2aa87defb5090549e26f5ef4b
andrei-cristea <andrei.cristea019@gmail.com>
2024-09-19 11:48:27 +0200

    fix: distributed backward compatibility for legacy keyple-less clients (#14)

--- src/main/java/org/eclipse/keyple/card/generic/CardRequestAdapter.java
+++ src/main/java/org/eclipse/keyple/card/generic/CardRequestAdapter.java
@@ -25,6 +25,7 @@ final class CardRequestAdapter implements CardRequestSpi {
 
   private final List<ApduRequestSpi> apduRequests;
   private final boolean stopOnUnsuccessfulStatusWord;
+  private final boolean isStatusCodesVerificationEnabled; // Only for JSON backward compatibility
 
   /**
    * Builds a card request with a list of {@link ApduRequestSpi } and the flag indicating the
@@ -41,6 +42,7 @@ final class CardRequestAdapter implements CardRequestSpi {
   CardRequestAdapter(List<ApduRequestSpi> apduRequests, boolean stopOnUnsuccessfulStatusWord) {
     this.apduRequests = apduRequests;
     this.stopOnUnsuccessfulStatusWord = stopOnUnsuccessfulStatusWord;
+    this.isStatusCodesVerificationEnabled = stopOnUnsuccessfulStatusWord;
   }
 
   /**
