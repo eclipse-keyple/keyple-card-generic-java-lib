55cc278b07bff14feca577443a1949a5009b9ea5
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-06-23 17:04:50 +0200

    ci: migrate pipeline from Jenkins to GitHub Actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
 rootProject.name = "keyple-card-generic-java-lib"
+
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+  }
+}
+
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    mavenLocal()
+    mavenCentral()
+  }
+}
