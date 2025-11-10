e363646f94b284bd62f05d8bc25ab18803dd417c
Andrei Cristea <andrei.cristea019@gmail.com>
2025-11-10 16:30:37 +0100

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,9 @@ plugins {
 ///////////////////////////////////////////////////////////////////////////////
 
 dependencies {
-  implementation("org.eclipse.keypop:keypop-reader-java-api:2.0.1")
+  implementation("org.eclipse.keypop:keypop-reader-java-api:2.1.0-SNAPSHOT") { isChanging = true }
   implementation("org.eclipse.keypop:keypop-card-java-api:2.0.1")
-  implementation("org.eclipse.keyple:keyple-common-java-api:2.0.1")
+  implementation("org.eclipse.keyple:keyple-common-java-api:2.0.2")
   implementation("org.eclipse.keyple:keyple-service-resource-java-lib:3.1.0")
   implementation("org.eclipse.keyple:keyple-util-java-lib:2.4.0")
   implementation("org.slf4j:slf4j-api:1.7.32")
